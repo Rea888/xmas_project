0c623b77fb2098b033b6b83c091a4da6b3761705
Viktoria Rakhely <m.rakhely.viktoria@gmail.com>
2022-11-13 22:43:50 +0100

    edited introduction text, deleted href from moth, added CodeImage controller colored icons in introduction text added isItXmasYet functiom to ForbidAccessBeforeChristmas middleware controller added environtment checking to isItXmasYet function changed metadata in moth.png

--- routes/web.php
+++ routes/web.php
@@ -3,27 +3,26 @@
 use App\Http\Controllers\CodeImage;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Middleware\ForbidAccessBeforeChristmas;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/introduction', function(){
-    return view('introduction');
-})->middleware(ForbidAccessBeforeChristmas::class);
+Route::middleware(ForbidAccessBeforeChristmas::class)->group(function () {
 
-Route::get('/second', function(){
-    return "second puzle";
+    Route::get('/introduction', function () {
+        return view('introduction');
+    });
+
+    Route::get('/second', function () {
+        return "second puzle";
+    });
+
 });
 
-Route::get('/code', [ForbidAccessBeforeChristmas::class, 'handle']);
+
+
+
+
+
+
