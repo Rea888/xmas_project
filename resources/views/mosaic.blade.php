08908393bdcb4d3c767b0146d81493fd7de3cb93
Viktoria Rakhely <m.rakhely.viktoria@gmail.com>
2022-12-15 17:03:44 +0100

    made mosaic responsive

--- resources/views/mosaic.blade.php
+++ resources/views/mosaic.blade.php
@@ -11,17 +11,15 @@
 
     <body id = "primary">
 
-
-    <div id="moj"><img class="alignnone size-medium wp-image-8" src="images/image_part_001.jpg" width="200" height="200" alt=""  />
-    </div>
-    <div id="moj"><img class="alignnone size-full wp-image-9" src="images/image_part_002.jpg" width="200" height="200" alt=""  /></div>
-    <div id="moj"><img class="alignnone size-medium wp-image-7" src="images/image_part_003.jpg" width="200" height="200" alt=""  /></div>
-    <div id="moj"><img class="alignnone size-medium wp-image-6" src="images/image_part_004.jpg" width="200" height="200" alt=""  /></div>
-    <div id="moj"><img class="alignnone size-medium wp-image-5" src="images/image_part_005.jpg" width="200" height="200" alt=""  /></div>
-    <div id="moj"><img class="alignnone size-medium wp-image-10" src="images/image_part_006.jpg" width="200" height="200" alt=""  /></div>
-    <div id="moj"><img class="alignnone size-medium wp-image-10" src="images/image_part_007.jpg" width="200" height="200" alt=""  /></div>
-    <div id="moj"><img class="alignnone size-medium wp-image-10" src="images/image_part_008.jpg" width="200" height="200" alt=""  /></div>
-    <div id="moj"><img class="alignnone size-medium wp-image-10" src="images/image_part_009.jpg" width="200" height="200" alt=""  /></div>
+    <div id="moj"><img class="alignnone size-medium wp-image-10" src="images/img_night.jpg" width="200" height="200" alt=""  /></div>
+    <div id="moj"><img class="alignnone size-medium wp-image-10" src="images/img_sin.jpg" width="200" height="200" alt=""  /></div>
+    <div id="moj"><img class="alignnone size-medium wp-image-7" src="images/img_turd.jpg" width="200" height="200" alt=""  /></div>
+    <div id="moj"><img class="alignnone size-medium wp-image-10" src="images/img_env.jpg" width="200" height="200" alt=""  /></div>
+    <div id="moj"><img class="alignnone size-medium wp-image-5" src="images/img_find.jpg" width="200" height="200" alt=""  /></div>
+    <div id="moj"><img class="alignnone size-full wp-image-9" src="images/img_twinkle.jpg" width="200" height="200" alt=""  /></div>
+    <div id="moj"><img class="alignnone size-medium wp-image-10" src="images/img_secret.jpg" width="200" height="200" alt=""  /></div>
+    <div id="moj"><img class="alignnone size-medium wp-image-8" src="images/img_orange.jpg" width="200" height="200" alt=""  /></div>
+    <div id="moj"><img class="alignnone size-medium wp-image-6" src="images/img_floor.jpg" width="200" height="200" alt=""  /></div>
 
     </body>
 
